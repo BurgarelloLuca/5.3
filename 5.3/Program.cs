2620f2d1595ad22cabc4064c1f274e64bec22ab0
BurgarelloLuca <burgarelloluca668@gmail.com>
2025-03-06 19:09:11 +0100

    Add project files.

---
+++ 5.3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _5._3
+{
+    enum seme
+    {
+        cuori = 0,
+        picche = 1,
+        fiori = 2,
+        quadri = 3,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("scegliere un seme");
+            string scelta = Console.ReadLine();
+            seme numCarta = (seme)Enum.Parse(typeof(seme), scelta);
+            Console.WriteLine((int)numCarta);
+        }
+    }
+}
